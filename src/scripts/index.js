b00f287d07de185d03768b47fe8e3914f25e6e98
erkhes.dc <kevin.tamir07@gmail.com>
2025-03-02 20:56:15 -0500

    About page

--- src/scripts/index.js
+++
@@ -1,6 +0,0 @@
-const navToggle = document.getElementById('nav-toggle');
-const navClose = document.getElementById('nav-close');
-const menuOverlay = document.querySelector('.nav-overlay');
-
-navToggle.addEventListener('click', () => menuOverlay.classList.add('show-overlay'));
-navClose.addEventListener('click', () => menuOverlay.classList.remove('show-overlay'));
